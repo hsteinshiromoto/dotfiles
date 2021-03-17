e884da51e52d5bfb2532748e4810e1e6dced6b16
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-03-17 21:32:35 +1100

    add comments

--- bin/oh_my_zsh.sh
+++ bin/oh_my_zsh.sh
@@ -1,5 +1,11 @@
+# Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
+# Clone dotfiles
 cd ~ && git clone https://github.com/hsteinshiromoto/dotfiles.git
+
+# Copy rc file
 cp ~/dotfiles/.zshrc ~/.zshrc
