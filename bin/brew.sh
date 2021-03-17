8ac99d8f80fb517fddfbd7dae81f3514e4725093
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-03-17 17:22:06 +1100

    add files

---
+++ bin/brew.sh
@@ -0,0 +1,16 @@
+# Install Brew Packages
+brew install python
+brew install tree
+brew install wget
+brew install zsh
+
+# Install MacOS Applications
+brew install --cask bitwarden
+brew install --cask docker
+brew install --cask firefox
+brew install --cask iterm2
+brew install --cask visual-studio-code
+
+# Fonts
+brew tap homebrew/cask-Fonts
+brew install --cask font-source-code-pro
