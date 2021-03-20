5a312603d0202a292589a8c0a6356738afb3eb04
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-03-20 12:10:05 +1100

    add more programs

--- bin/brew.sh
+++ bin/brew.sh
@@ -5,22 +5,29 @@
 brew install gpg
 brew install pinentry-mac #To be used with gpg
 brew install python
+brew install texlive
 brew install tree
 brew install wget
 brew install zsh
 
 # Install MacOS Applications
+brew install --cask adobe-acrobat-reader
 brew install --cask alfred
 brew install --cask bartender
 brew install --cask bettertouchtool
+brew install --cask bibdesk
 brew install --cask bitwarden
 brew install --cask boxcryptor
+brew install --cask daisydisk
 brew install --cask dbeaver-community
 brew install --cask docker
+brew install --cask drawio
 brew install --cask dropbox
 brew install --cask firefox
+brew install --cask inkscape
 brew install --cask istat-menus
 brew install --cask iterm2
+brew install --cask microsoft-office
 brew install --cask notion
 brew install --cask obsidian
 brew install --cask pocket-casts
@@ -31,7 +38,11 @@ brew install --cask slack
 brew install --cask spotify
 brew install --cask standard-notes
 brew install --cask sublime-merge
+brew install --cask the-unarchiver
+brew install --cask transmission
 brew install --cask visual-studio-code
+brew install --cask vlc
+brew install --cask waterfox
 brew install --cask whatsapp
 brew install --cask xquartz
 
