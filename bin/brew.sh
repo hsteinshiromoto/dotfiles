1f4ab6edf0000c32ae645d2a2b3f7a4115678efc
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-03-20 11:45:17 +1100

    add more programs

--- bin/brew.sh
+++ bin/brew.sh
@@ -13,10 +13,19 @@ brew install --cask bitwarden
 brew install --cask docker
 brew install --cask firefox
 brew install --cask iterm2
+brew install --cask notion
+brew install --cask pocket-casts
 brew install --cask protonvpn
 brew install --cask signal
+brew install --cask spotify
+brew install --cask sublime-merge
+brew install --cask xquartz
 brew install --cask visual-studio-code
 
 # Fonts
 brew tap homebrew/cask-Fonts
 brew install --cask font-source-code-pro
+
+# Xfig
+brew tap mistydemeo/homebrew-xfig
+brew install xfig
