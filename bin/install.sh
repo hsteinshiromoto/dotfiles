f2bf1a22f3eb3101c4a61200fd6dc7ae32b0d8d8
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-05-10 08:40:26 +1000

    Improve installation

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+(cd ~/Downloads && bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+(bash ~/Downloads/install.sh)
+
 bash brew.sh
-
 bash oh_my_zsh.sh
-
 bash docker.sh
