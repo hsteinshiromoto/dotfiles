a49610060d20d2a47561691698f6e5a6b285d7af
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2022-09-22 21:43:23 +1000

    add mc

---
+++ bin/mc.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+for file in ini mc.ext panels.ini hotlist
+do
+    echo copying ${file}
+    cp ~/.config/mc/${file} ${PROJECT_ROOT}/mc
+done
