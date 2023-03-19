836479b6a612896f1034b0f50fa01eb603cea643
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2023-03-19 22:05:55 +1100

    include file to backup

--- bin/mc.sh
+++ bin/mc.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
-for file in ini mc.ext panels.ini hotlist
+for file in ini mc.ext mc.ext.ini panels.ini hotlist
 do
     echo copying ${file}
     cp ~/.config/mc/${file} ${PROJECT_ROOT}/mc
