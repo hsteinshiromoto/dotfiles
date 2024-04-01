b7f4a4b12d7c9d49191cfb10e07da0f35205a760
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-04-01 22:25:22 +1100

    export common functions to base.sh

--- bin/configure.sh
+++ bin/configure.sh
@@ -33,48 +33,7 @@ fi
 VAULT_SECRET="$HOME/.ansible-vault/vault.secret"
 IS_FIRST_RUN="$HOME/.dotfiles_run"
 
-function _task {
-  # if _task is called while a task was set, complete the previous
-  if [[ $TASK != "" ]]; then
-    printf "${OVERWRITE}${LGREEN} [✓]  ${LGREEN}${TASK}\n"
-  fi
-  # set new task title and print
-  TASK=$1
-  printf "${LBLACK} [ ]  ${TASK} \n${LRED}"
-}
-
-# _cmd performs commands with error checking
-function _cmd {
-  #create log if it doesn't exist
-  if ! [[ -f $DOTFILES_LOG ]]; then
-    touch $DOTFILES_LOG
-  fi
-  # empty conduro.log
-  > $DOTFILES_LOG
-  # hide stdout, on error we print and exit
-  if eval "$1" 1> /dev/null 2> $DOTFILES_LOG; then
-    return 0 # success
-  fi
-  # read error from log and add spacing
-  printf "${OVERWRITE}${LRED} [X]  ${TASK}${LRED}\n"
-  while read line; do
-    printf "      ${line}\n"
-  done < $DOTFILES_LOG
-  printf "\n"
-  # remove log file
-  rm $DOTFILES_LOG
-  # exit installation
-  exit 1
-}
-
-function _clear_task {
-  TASK=""
-}
-
-function _task_done {
-  printf "${OVERWRITE}${LGREEN} [✓]  ${LGREEN}${TASK}\n"
-  _clear_task
-}
+source base.sh
 
 if ! [[ -d "$DOTFILES_DIR" ]]; then
   _task "Cloning repository"
