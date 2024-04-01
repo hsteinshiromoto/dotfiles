540930614b8d5a6708fb300539daec25804d55b7
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-04-01 23:04:44 +1100

    Improve file by adding ansible galaxy install command

--- bin/install.sh
+++ bin/install.sh
@@ -4,11 +4,11 @@ unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)     machine=Linux;;
     Darwin*)    machine=Mac;;
-    CYGWIN*)    machine=Cygwin;;
-    MINGW*)     machine=MinGw;;
-    MSYS_NT*)   machine=Git;;
     *)          machine="UNKNOWN:${unameOut}"
 esac
+
+set +e
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
 if [ $machine == "Mac" ]; then
     echo "Running on Mac"
@@ -16,22 +16,15 @@ if [ $machine == "Mac" ]; then
     curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | sudo bash
     # Install Ansible
     brew install ansible
-    # Add Ansible to PATH
-    ansible-galaxy install -r ansible-requirements.yml
-    
+
 elif [ $machine == "Linux" ]; then
-    echo "Running on Linux"
+    _task "Running on Linux"
     # Install Ansible
     sudo apt update
     sudo apt install ansible
-    # Install Ansible roles
-    ansible-galaxy install -r ansible-requirements.yml
+
 else
     echo "Unsupported OS"
 fi
 
-
-# pip3 install ansible
-
-# export PATH="$PATH:~/.local/bin"
-# ansible-galaxy install -r ansible-requirements.yml
+ansible-galaxy install -r ${PROJECT_ROOT}/ansible-requirements.txt
