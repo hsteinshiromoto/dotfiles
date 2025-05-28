f3b89b1e02959bf427101441b55d89190b2751c0
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-05-28 22:38:00 +1000

    Remove other linters/formatters for Python

--- .config/nvim/lua/plugins/extras/lang/python.lua
+++ .config/nvim/lua/plugins/extras/lang/python.lua
@@ -10,32 +10,12 @@ return {
 		"neovim/nvim-lspconfig",
 		opts = {
 			servers = {
-				pyright = {
-					settings = {
-						python = {
-							analysis = {
-								typeCheckingMode = "basic",
-								autoSearchPaths = true,
-								useLibraryCodeForTypes = true,
-								diagnosticMode = "workspace",
-							},
-						},
-					},
-				},
 				ruff_lsp = {},
 			},
 			setup = {
-				pyright = function(_, opts)
-					local lsp_utils = require("plugins.lsp.utils")
-					lsp_utils.on_attach(function(client, buffer)
-            -- stylua: ignore
-            if client.name == "pyright" then
-              vim.keymap.set("n", "<leader>tC", function() require("dap-python").test_class() end, { buffer = buffer, desc = "Debug Class" })
-              vim.keymap.set("n", "<leader>tM", function() require("dap-python").test_method() end, { buffer = buffer, desc = "Debug Method" })
-              vim.keymap.set("v", "<leader>tS", function() require("dap-python").debug_selection() end, { buffer = buffer, desc = "Debug Selection" })
-            end
-					end)
-				end,
+				-- No specific setup for ruff_lsp needed here by default
+				-- The general on_attach from plugins.lsp.utils will apply if it exists
+				-- or the default nvim-lspconfig on_attach will be used.
 			},
 		},
 	},
