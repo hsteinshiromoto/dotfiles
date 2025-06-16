3bc7b49e5aeb844c059c534c587e8eabc3eac9e8
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-06-16 22:25:28 +1000

    Improve harpoon with correct keywords

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -2,6 +2,7 @@ return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+	lazy = false,
 
 	config = function()
 		local harpoon = require("harpoon")
@@ -27,8 +28,36 @@ return {
 				:find()
 		end
 
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end, { desc = "Add file to harpoon" })
+
 		vim.keymap.set("n", "<C-e>", function()
 			toggle_telescope(harpoon:list())
 		end, { desc = "Open harpoon window" })
+
+		vim.keymap.set("n", "<leader>1", function()
+			harpoon:list():select(1)
+		end, { desc = "Go to harpoon file 1" })
+
+		vim.keymap.set("n", "<leader>2", function()
+			harpoon:list():select(2)
+		end, { desc = "Go to harpoon file 2" })
+
+		vim.keymap.set("n", "<leader>3", function()
+			harpoon:list():select(3)
+		end, { desc = "Go to harpoon file 3" })
+
+		vim.keymap.set("n", "<leader>4", function()
+			harpoon:list():select(4)
+		end, { desc = "Go to harpoon file 4" })
+
+		-- Toggle previous & next buffers stored within Harpoon list
+		vim.keymap.set("n", "<C-S-P>", function()
+			harpoon:list():prev()
+		end)
+		vim.keymap.set("n", "<C-S-N>", function()
+			harpoon:list():next()
+		end)
 	end,
 }
