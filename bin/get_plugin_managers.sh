604b52f5b752f8ca02e4cfc35ab62568922fb092
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-06-29 22:21:45 +1000

    bash file to get plugin managers

---
+++ bin/get_plugin_managers.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+# ---
+# Functions
+# ---
+
+# Documentation
+display_help() {
+    echo "Usage: [variable=value] $0" >&2
+    echo
+    echo "   -t, --tpm      tmux plugin manager"
+    echo "   -h, --help     display help"
+    echo "   -z, --zgenom   zgenom plugin manager"
+    echo
+    # echo some stuff here for the -a or --add-options
+    exit 1
+}
+
+zgenom() {
+	git clone https://github.com/jandamm/zgenom.git "${HOME}/.zgenom"
+}
+
+tpm() {
+	git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
+}
+
+# Available options
+while :
+do
+    case "$1" in
+        -z | --zgenom)
+            enter_container
+            exit 0
+            ;;
+
+        -h | --help)
+            display_help
+            exit 0
+            ;;
+
+
+        -t | --tpm)
+            kill_container
+            exit 0
+            ;;
+
+       "")
+            display_help  # Call your function
+            break
+            ;;
+
+        --) # End of all options
+            shift
+            break
+            ;;
+        -*)
+            echo "Error: Unknown option: $1" >&2
+            ## or call function display_help
+            exit 1
+            ;;
+
+      *)  # No more options
+            break
+            ;;
+    esac
+done
