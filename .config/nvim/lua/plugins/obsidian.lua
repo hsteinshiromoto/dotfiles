2d565448011b841ba0c87f4558b126e62aec7b86
Humberto STEIN SHIROMOTO <humberto.shiromoto@akordi.com>
2025-08-01 16:52:06 +1000

    Use stable version of obsidian.nvim

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -28,7 +28,6 @@ end
 
 return {
 	"obsidian-nvim/obsidian.nvim",
-	version = "*", -- recommended, use latest release instead of latest commit
 	lazy = true,
 	ft = "markdown",
 	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
