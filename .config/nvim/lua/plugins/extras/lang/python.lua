4cad3b0952bef489bc8880cc33ea54294e9bdb68
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-08-03 19:10:13 +1000

    Add pytest nvim plugin

--- .config/nvim/lua/plugins/extras/lang/python.lua
+++ .config/nvim/lua/plugins/extras/lang/python.lua
@@ -74,6 +74,18 @@ return {
 			},
 		},
 	},
+	{
+		"richardhapb/pytest.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {}, -- Define the options here
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "python", "xml" },
+			})
+
+			require("pytest").setup(opts)
+		end,
+	},
 }
 -- References:
 -- 	[1] https://alpha2phi.medium.com/modern-neovim-debugging-and-testing-8deda1da1411
