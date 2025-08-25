e52468c9637eb0020aee8170a9c37bca941a0f8a
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-08-26 08:58:44 +1000

    Add mini.sessions and integrate with dat

--- .config/nvim/lua/plugins/extras/ui/dart.lua
+++ .config/nvim/lua/plugins/extras/ui/dart.lua
@@ -5,5 +5,17 @@ return {
 		"echasnovski/mini.nvim", -- optional, icons provider
 		"nvim-tree/nvim-web-devicons", -- optional, icons provider
 	},
+	require("mini.sessions").setup({
+		hooks = {
+			pre = {
+				read = function(session)
+					Dart.read_session(session["name"])
+				end,
+				write = function(session)
+					Dart.write_session(session["name"])
+				end,
+			},
+		},
+	}),
 	opts = {}, -- see Configuration section
 }
