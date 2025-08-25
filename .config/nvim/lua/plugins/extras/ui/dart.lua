67764653dc7a06fc3a8c558b6bf9301c80211921
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-08-26 08:40:08 +1000

    Add dart.nvim

---
+++ .config/nvim/lua/plugins/extras/ui/dart.lua
@@ -0,0 +1,8 @@
+return {
+	"iofq/dart.nvim",
+	dependencies = {
+		"echasnovski/mini.nvim", -- optional, icons provider
+		"nvim-tree/nvim-web-devicons", -- optional, icons provider
+	},
+	opts = {}, -- see Configuration section
+}
