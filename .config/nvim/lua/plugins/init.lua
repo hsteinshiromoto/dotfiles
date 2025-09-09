7b438ba677607758d50f31de6a0f2abb642f6f49
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-09-09 21:20:43 +1000

    Change keymap for togging undo tree

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -47,7 +47,7 @@ return {
 		"mbbill/undotree",
 		lazy = false,
 		config = function()
-			vim.keymap.set("n", "<C-u>", vim.cmd.UndotreeToggle, { desc = "Toggle Undo Tree" })
+			vim.keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle, { desc = "Toggle Undo Tree" })
 		end,
 	},
 	-- session management
