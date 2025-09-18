ce32b88929700920e72a0bf657ea2a7800f723ce
Humberto STEIN SHIROMOTO <humberto.shiromoto@akordi.com>
2025-09-18 15:00:03 +1000

    PROJ-123: Add meow.yarn

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -111,4 +111,32 @@ return {
 		},
 		config = true,
 	},
+	{
+		"retran/meow.yarn.nvim",
+		lazy = false,
+		dependencies = { "MunifTanjim/nui.nvim" },
+		config = function()
+			require("meow.yarn").setup({
+				-- Using lua functions
+				vim.keymap.set("n", "<leader>yt", function()
+					require("meow.yarn").open_tree("type_hierarchy", "supertypes")
+				end, { desc = "Yarn: Type Hierarchy (Super)" }),
+				vim.keymap.set("n", "<leader>yT", function()
+					require("meow.yarn").open_tree("type_hierarchy", "subtypes")
+				end, { desc = "Yarn: Type Hierarchy (Sub)" }),
+				vim.keymap.set("n", "<leader>yc", function()
+					require("meow.yarn").open_tree("call_hierarchy", "callers")
+				end, { desc = "Yarn: Call Hierarchy (Callers)" }),
+				vim.keymap.set("n", "<leader>yC", function()
+					require("meow.yarn").open_tree("call_hierarchy", "callees")
+				end, { desc = "Yarn: Call Hierarchy (Callees)" }),
+
+				-- Or using commands
+				vim.keymap.set("n", "<leader>yS", "<Cmd>MeowYarn type super<CR>", { desc = "Yarn: Super Types" }),
+				vim.keymap.set("n", "<leader>ys", "<Cmd>MeowYarn type sub<CR>", { desc = "Yarn: Sub Types" }),
+				vim.keymap.set("n", "<leader>yC", "<Cmd>MeowYarn call callers<CR>", { desc = "Yarn: Callers" }),
+				vim.keymap.set("n", "<leader>yc", "<Cmd>MeowYarn call callees<CR>", { desc = "Yarn: Callees" }),
+			})
+		end,
+	},
 }
