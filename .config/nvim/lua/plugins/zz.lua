b51a33fce4e26892a7dc4d5fe7492c4e9ac6a344
Humberto STEIN SHIROMOTO <humberto.shiromoto@akordi.com>
2025-10-28 09:49:02 +1100

    PROJ-123: Add zz.nvim and yazi.nvim

---
+++ .config/nvim/lua/plugins/zz.lua
@@ -0,0 +1,10 @@
+return {
+	"db757/zz.nvim",
+	lazy = false,
+	dependencies = {
+		-- Optional dependencies
+		"folke/which-key.nvim",
+		"folke/snacks.nvim",
+	},
+	-- Optional configuration (plugin auto-initializes with defaults)
+}
