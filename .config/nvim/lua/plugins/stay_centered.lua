d282510a404bec14edf39534b31a4e0b4b2f1d1f
Humberto STEIN SHIROMOTO <humberto.shiromoto@akordi.com>
2025-10-28 16:53:29 +1100

    PROJ-123: Skip telescope and snacks inputs and pickers

--- .config/nvim/lua/plugins/stay_centered.lua
+++ .config/nvim/lua/plugins/stay_centered.lua
@@ -2,8 +2,9 @@ return {
 	'arnamak/stay-centered.nvim',
 	lazy = false,
 	config = function()
-		require("outline").setup({
-			vim.keymap.set({ 'n', 'v' }, '<leader>cc', require('stay-centered').toggle, { desc = 'Toggle stay-centered.nvim' })
+		require("stay-centered").setup({
+			skip_filetypes = { 'TelescopePrompt', 'snacks_input', 'snacks_picker', 'snacks_picker_list' },
 		})
+		vim.keymap.set({ 'n', 'v' }, '<leader>cc', require('stay-centered').toggle, { desc = 'Toggle stay-centered.nvim' })
 	end
 }
