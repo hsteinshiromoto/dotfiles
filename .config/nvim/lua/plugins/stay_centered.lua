dfed323ea3e66061dfeb983df38b706e207bc81a
Humberto STEIN SHIROMOTO <humberto.shiromoto@akordi.com>
2025-11-20 13:46:38 +1100

    Change keymap for stay_centered nvim plugin

--- .config/nvim/lua/plugins/stay_centered.lua
+++ .config/nvim/lua/plugins/stay_centered.lua
@@ -5,6 +5,6 @@ return {
 		require("stay-centered").setup({
 			skip_filetypes = { 'TelescopePrompt', 'snacks_input', 'snacks_picker', 'snacks_picker_list' },
 		})
-		vim.keymap.set({ 'n', 'v' }, '<leader>cc', require('stay-centered').toggle, { desc = 'Toggle stay-centered.nvim' })
+		vim.keymap.set({ 'n', 'v' }, '<leader>ac', require('stay-centered').toggle, { desc = 'Toggle stay-centered.nvim' })
 	end
 }
