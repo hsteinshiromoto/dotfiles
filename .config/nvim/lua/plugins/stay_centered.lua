36075b20da632f1162be5e23b02d45c525e970d7
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2026-01-09 16:18:45 +1100

    Add new keymap for nvim's stay-centered plugin

--- .config/nvim/lua/plugins/stay_centered.lua
+++ .config/nvim/lua/plugins/stay_centered.lua
@@ -5,6 +5,11 @@ return {
 		require("stay-centered").setup({
 			skip_filetypes = { 'TelescopePrompt', 'snacks_input', 'snacks_picker', 'snacks_picker_list' },
 		})
-		vim.keymap.set({ 'n', 'v' }, '<leader>ac', require('stay-centered').toggle, { desc = 'Toggle stay-centered.nvim' })
+		vim.keymap.set({ 'n', 'v' }, '<leader>ac', require('stay-centered').toggle,
+			{ desc = 'Toggle stay centered.nvim vertical alignment' })
+		vim.keymap.set({ 'n', 'v' }, '<leader>{', require('stay-centered').disable,
+			{ desc = 'Disable stay-centered.nvim vertical alignment' })
+		vim.keymap.set({ 'n', 'v' }, '<leader>}', require('stay-centered').disable,
+			{ desc = 'Disable stay-centered.nvim vertical alignment' })
 	end
 }
