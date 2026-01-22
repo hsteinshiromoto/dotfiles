7f730c35d58b6be75a837f1798a94b59a796a5e8
Humberto STEIN SHIROMOTO <humberto.shiromoto@akordi.com>
2026-01-22 16:04:20 +1100

    Change characters for buffer selection in dart.nvim

--- .config/nvim/lua/plugins/extras/ui/dart.lua
+++ .config/nvim/lua/plugins/extras/ui/dart.lua
@@ -2,7 +2,7 @@ return {
 	"iofq/dart.nvim",
 	lazy = false,
 	dependencies = {
-		"echasnovski/mini.nvim", -- optional, icons provider
+		"echasnovski/mini.nvim",     -- optional, icons provider
 		"nvim-tree/nvim-web-devicons", -- optional, icons provider
 	},
 	require("mini.sessions").setup({
@@ -20,20 +20,20 @@ return {
 	opts = {
 		-- List of characters to use to mark 'pinned' buffers
 		-- The characters will be chosen for new pins in order
-		marklist = { "1", "2", "3", "4", "5" },
+		marklist = { "q", "w", "e", "r", "t", "y" },
 
 		-- List of characters to use to mark recent buffers, which are displayed first (left) in the tabline
 		-- Buffers that are 'marked' are not included in this list
 		-- The length of this list determines how many recent buffers are tracked
 		-- Set to {} to disable recent buffers in the tabline
-		buflist = { "6", "7", "8", "9" },
+		buflist = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" },
 		mappings = {
-			mark = "<leader>a", -- Mark current buffer
-			jump = "<leader>", -- Jump to buffer marked by next character i.e `;a`
-			pick = "<leader>bl", -- Open Dart.pick
+			mark = "<leader>a",     -- Mark current buffer
+			jump = "<leader>",      -- Jump to buffer marked by next character i.e `;a`
+			pick = "<leader>bl",    -- Open Dart.pick
 			next = "<leader><Right>", -- Cycle right through the tabline
 			prev = "<leader><Left>", -- Cycle left through the tabline
 			unmark_all = "<leader>bu", -- Close all marked and recent buffers
 		},
-	}, -- see Configuration section
+	},                          -- see Configuration section
 }
