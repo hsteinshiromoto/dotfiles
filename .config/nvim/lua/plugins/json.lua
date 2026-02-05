d867cd9f043bc0d34166dea079060692bee605d1
Humberto STEIN SHIROMOTO <humberto.shiromoto@akordi.com>
2026-02-05 11:06:24 +1100

    Set json-nvim lazy and file type settings

--- .config/nvim/lua/plugins/json.lua
+++ .config/nvim/lua/plugins/json.lua
@@ -1,5 +1,7 @@
 return {
 	"VPavliashvili/json-nvim",
+	lazy = false,
+	ft = "json",
 	config = function()
 		vim.keymap.set("n", "<leader>jff", '<cmd>JsonFormatFile<cr>')
 		vim.keymap.set("n", "<leader>jmf", '<cmd>JsonMinifyFile<cr>')
