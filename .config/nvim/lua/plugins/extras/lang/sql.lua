c6cc87641c4d69f0cfe0f198e9df90f957ff65c1
Humberto STEIN SHIROMOTO <humberto.shiromoto@akordi.com>
2026-03-23 09:24:13 +1100

    Improve settings for sql.lua

--- .config/nvim/lua/plugins/extras/lang/sql.lua
+++ .config/nvim/lua/plugins/extras/lang/sql.lua
@@ -2,5 +2,26 @@ return {
 	'xemptuous/sqlua.nvim',
 	lazy = false,
 	cmd = 'SQLua',
-	config = function() require('sqlua').setup() end
+	config = function()
+		require('sqlua').setup({
+			-- the parent folder that databases will be placed, holding
+			-- various tmp files and other saved queries.
+			db_save_location = "~/.local/share/nvim/sqlua/",
+			-- where to save the json config containing connection information
+			connections_save_location = "~/.local/share/nvim/sqlua/connections.json",
+			-- the default limit attached to queries
+			-- currently only works on "Data" command under a table
+			default_limit = 1000,
+			-- whether to introspect the database on SQLua open or when first expanded
+			-- through the sidebar
+			load_connections_on_start = false,
+			keybinds = {
+				execute_query = "<leader>r",
+				activate_db = "<C-a>",
+
+				-- Execute query (just like keybinds.execute_query) while in insert mode for query
+				insert_execute_query = "<C-r>",
+			}
+		})
+	end
 }
