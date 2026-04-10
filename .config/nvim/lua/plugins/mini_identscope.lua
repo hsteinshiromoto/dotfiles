ec3b7ad6bc90faeb0673f2935e0bbe4d40e87d58
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2026-04-10 19:35:28 +1000

    Add color to the indent scope

--- .config/nvim/lua/plugins/mini_identscope.lua
+++ .config/nvim/lua/plugins/mini_identscope.lua
@@ -1,1 +1,14 @@
-return { 'echasnovski/mini.indentscope', version = '*', lazy = false, opts = { symbol = '│' } }
+return {
+	'echasnovski/mini.indentscope',
+	version = '*',
+	lazy = false,
+	config = function()
+		require('mini.indentscope').setup({ symbol = '│' })
+		vim.api.nvim_set_hl(0, 'MiniIndentscopeSymbol', { fg = '#2b2f4b' })
+		vim.api.nvim_create_autocmd('ColorScheme', {
+			callback = function()
+				vim.api.nvim_set_hl(0, 'MiniIndentscopeSymbol', { fg = '#2b2f4b' })
+			end,
+		})
+	end,
+}
