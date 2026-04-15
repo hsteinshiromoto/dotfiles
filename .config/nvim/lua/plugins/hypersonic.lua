5ef6614b04fe8a2fe1cf31bf875721e8bc9eaff4
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2026-04-15 22:17:34 +1000

    Add keymap to select regex and explain it

--- .config/nvim/lua/plugins/hypersonic.lua
+++ .config/nvim/lua/plugins/hypersonic.lua
@@ -6,5 +6,11 @@ return {
 		require('hypersonic').setup({
 			-- config
 		})
+		vim.keymap.set('v', '<leader>h', function()
+			local start_pos = vim.fn.getpos("'<")
+			local end_pos = vim.fn.getpos("'>")
+			local lines = vim.fn.getregion(start_pos, end_pos)
+			vim.cmd('Hypersonic ' .. table.concat(lines, ''))
+		end, { desc = 'Explain selected regex with Hypersonic' })
 	end
 }
