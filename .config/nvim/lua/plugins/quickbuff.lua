91513710081f7fd0d1c479db5f2a3eabd561fa79
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2026-06-15 20:03:55 +1000

    Configure quickbuf

--- .config/nvim/lua/plugins/quickbuff.lua
+++ .config/nvim/lua/plugins/quickbuff.lua
@@ -1,6 +1,11 @@
 return {
 	"tjgao/quickbuf.nvim",
+	lazy = false,
 	config = function()
 		require("quickbuf").setup()
 	end,
+	vim.keymap.set("n", "<leader>bb", "<cmd>QuickBuf<CR>", { desc = "QuickBuf" }),
+	vim.keymap.set("n", "<leader>qt", "<cmd>QuickBufPinToggle<CR>", { desc = "Pin toggle" }),
+	vim.keymap.set("n", "<S-h>", "<cmd>QuickBufPrevPinned<CR>", { desc = "Prev pinned buffer" }),
+	vim.keymap.set("n", "<S-l>", "<cmd>QuickBufNextPinned<CR>", { desc = "Next pinned buffer" })
 }
